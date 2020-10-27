97f8192c3a7a0915fa66c7e169fe8510e75e57fa
cotrieuphong <co.trieu.phong@sun-asterisk.com>
2020-10-27 17:10:32 +0700

    Japanese JSON

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,12 @@
 <template lang="pug">
-  v-card.overflow-hidden
-    v-app-bar(absolute='' color='white' elevate-on-scroll='' scroll-target='#scrolling-techniques-7')
-      v-app-bar-nav-icon
-      v-toolbar-title Title
-      v-spacer
-      v-btn(icon='')
-        v-icon mdi-magnify
-      v-btn(icon='')
-        v-icon mdi-heart
-      v-btn(icon='')
-        v-icon mdi-dots-vertical
-    v-sheet#scrolling-techniques-7.overflow-y-auto(max-height='600')
-      v-container(style='height: 1500px;')
-        nuxt-link(to='/') Cheatsheet
-        nuxt-link.red(to='/memorize') Memorize
-        Nuxt
+  v-container
+    v-app-bar(flat dense)
+      v-toolbar-title {{ title }}
+      v-btn(to='/') Cheatsheet
+      v-btn(to='/memorize') Memorize
+    transition(transition='scroll-y-transition')
+      Nuxt
+
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
 <template lang="pug">
-  div
-    p Yoooo !!
+  v-row
+    v-col(cols='12' md='6' lg='4')
+      h2.title Hiragana
+    v-col(cols='12' md='6' lg='4') Katakana
+    v-col(cols='12' md='6' lg='4') Kanji
 </template>
 
 <script>
-export default {}
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState({
+      state: (state) => state,
+    }),
+  },
+  mounted() {
+    console.log(this.state)
+  },
+}
 </script>
